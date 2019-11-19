085b246be6fa76aaa71ba60265f9141937a700d0
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-11-19 00:06:10 +0000

    Alter footer from defaults

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,30 +37,33 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('introduction', this.props.language)}>
+              Getting Started
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
+            <a href={this.docUrl('components/action-bar', this.props.language)}>
+              UI Components Reference
+            </a>
+            {/*<a href={this.docUrl('doc2.html', this.props.language)}>
               Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
+            </a>*/}
+            {/*<a href={this.docUrl('doc3.html', this.props.language)}>
               API Reference (or other categories)
-            </a>
+            </a>*/}
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
+            {/*<a href={this.pageUrl('users.html', this.props.language)}>
               User Showcase
-            </a>
+            </a>*/}
             <a
-              href="https://stackoverflow.com/questions/tagged/"
+              href="https://stackoverflow.com/questions/tagged/react-nativescript"
               target="_blank"
               rel="noreferrer noopener">
               Stack Overflow
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
+            <a href="https://nativescriptcommunity.slack.com/"><span>Chat on Slack in </span><code>#react</code></a>
             <a
-              href="https://twitter.com/"
+              href="https://twitter.com/LinguaBrowse"
               target="_blank"
               rel="noreferrer noopener">
               Twitter
@@ -68,7 +71,7 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
+            {/* <a href={`${this.props.config.baseUrl}blog`}>Blog</a> */}
             <a href="https://github.com/">GitHub</a>
             <a
               className="github-button"
@@ -107,18 +110,6 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
-          href="https://opensource.facebook.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
